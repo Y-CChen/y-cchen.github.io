7ff885ba6b007d176ed3a9d985a8db4631c0f6f0
Y-CChen <250f42mt@duck.com>
2024-11-22 18:18:29 +0800

    initialize project with basic configuration, environment variables, and license

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Y-CChen.github.io',
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      htmlAttrs: { lang: 'en' },
+    },
+  },
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      // automatically set at runtime using process.env.NUXT_PUBLIC_*
+    },
+    // automatically set at runtime using process.env.NUXT_*
+  },
+  telemetry: { enabled: false },
+});
